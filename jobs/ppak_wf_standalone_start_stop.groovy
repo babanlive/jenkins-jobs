cc2fbd6a9eba7c65277f23a7b5aa7972ea1d81b0
Mikhail Artemov <MArtemov@lanit.ru>
2025-11-17 22:16:19 +0700

    create folder

--- jobs/ppak_wf_standalone_start_stop.groovy
+++ jobs/ppak_wf_standalone_start_stop.groovy
@@ -1,21 +1,31 @@
+folder('Functions') {
+    displayName('Utility Functions')
+    description('Utility and maintenance jobs')
+}
+
 pipelineJob('Functions/ppak-wf-standalone-start-stop') {
     definition {
         cpsScm {
             scm {
                 git {
-                browser {}
-                remote {
-                    url('git@github.com:babanlive/jenkins-jobs.git')
-                }
-                branch('*/ppak-wf-standalone-start-stop')
+                    remote {
+                        url('git@github.com:babanlive/jenkins-jobs.git')
+                        credentials('github_babanlive_ssh')
+                    }
+                    branch('*/ppak-wf-standalone-start-stop')
                 }
             }
-            lightweight()
+            scriptPath('Jenkinsfile')
         }
     }
+    
     parameters {
         choiceParam('ACTION', ['start', 'stop'], 'Выбор действия с Wildfly: start/stop')
         stringParam('STAND', 'ppak', 'Выбор стенда (ppak)')
         stringParam('TAGS', 'saiku', 'Выбор тэга ansible (saiku - для выбора хостов ppak-app-wf-saiku, иначе оставить пустым)')
+    }
+    
+    logRotator {
+        numToKeep(5)
     }
 }

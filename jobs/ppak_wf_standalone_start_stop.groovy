140b1dfc460268e2c1d184adedf4e15816e23ed9
Mikhail Artemov <MArtemov@lanit.ru>
2026-02-07 21:03:53 +0700

    update Jenkinsfile - add new gitlab repository

--- jobs/ppak_wf_standalone_start_stop.groovy
+++ jobs/ppak_wf_standalone_start_stop.groovy
@@ -7,23 +7,31 @@ pipelineJob('Functions/ppak-wf-standalone-start-stop') {
         cpsScm {
             scm {
                 git {
+                    browser {}
                     remote {
-                        url('git@github.com:babanlive/jenkins-jobs.git')
-                        credentials('github_babanlive_ssh')
+                        url('git@gitlab.on1x-pwnz.ru:babanlive/ipr_jenkins.git')
+                        credentials('CredID_SSH_Jenkins_Masters_to_Gitlab')
+                    }
+                    extensions {
+                        cloneOptions {
+                            reference('/u01/jenkins_cache/git/hcs_jenkins.git')
+                            shallow(false)
+                            noTags(false)
+                            timeout(10)
+                        }
                     }
                     branch('*/ppak-wf-standalone-start-stop')
                 }
             }
-            scriptPath('Jenkinsfile')
+            lightweight()
         }
     }
-    
     parameters {
+        booleanParam('DRY_RUN', true, 'Если отмечено - Ansible запустится с флагом --check (без изменений на сервере)')
         choiceParam('ACTION', ['start', 'stop'], 'Выбор действия с Wildfly: start/stop')
-        stringParam('STAND', 'ppak', 'Выбор стенда (ppak)')
-        choiceParam('TAGS', ['', 'saiku'], 'Выбор тэга ansible (saiku - для выбора хостов ppak-app-wf-saiku, иначе оставить пустым)')
+        stringParam('STAND', 'ppak', 'Выбор стенда')
+        choiceParam('TAGS', ['', 'saiku'], 'Оставить пустым - для выбора всех ячеек; SAIKU - выбрать только ячейку saiku')
     }
-    
     logRotator {
         numToKeep(5)
     }
